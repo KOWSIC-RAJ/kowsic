8ff28a72ab7e2c7babe571e18756bad5234b6d48
KOWSIC-RAJ <kowsickraj17@gmail.com>
2017-06-21 12:25:21 +0530

    Add files via upload

---
+++ RemoveDuplicates.java
@@ -0,0 +1,29 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class RemoveDuplicates {
+	public static void main(String args[]){
+		Scanner s=new Scanner(System.in);
+	String str=s.next();
+	String st="";
+	int i,j;
+	int n=str.length();
+	char[] ch=str.toCharArray();
+	for( i=0;i<n;i++){
+		for(j=i+1;j<n;j++){
+			if(ch[i]==ch[j]){
+				ch[j]='\0';
+		}
+	}
+	}
+	for(i=0;i<n;i++){
+		if(ch[i]!='\0'){
+			st=st+ch[i];
+		}
+	}
+		System.out.println(st);
+	}
+
+}
+
