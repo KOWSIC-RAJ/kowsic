950dd41364ef21ec5b794deb37a4810415e35bea
KOWSIC-RAJ <kowsickraj17@gmail.com>
2017-07-03 16:50:26 +0530

    Add files via upload

---
+++ Chocolate.java
@@ -0,0 +1,14 @@
+package guvi;
+import java.util.Scanner;
+public class Chocolate {
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int m1=s.nextInt();
+		int n1=s.nextInt();
+		int m2=s.nextInt();
+		int n2=s.nextInt();
+		System.out.println(m1*n1-1);	
+		System.out.println(m2*n2-1);	
+	}
+
+}
