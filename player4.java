bba7929bc820860b9f30f27257bd3ed95a0145a0
KOWSIC-RAJ <kowsickraj17@gmail.com>
2017-07-27 09:20:08 +0100

    Add files via upload

---
+++ player4.java
@@ -0,0 +1,14 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class player4 {
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		String str=s.next();
+		int a=Integer.parseInt(str);
+		System.out.println(a);
+		
+	}
+
+}
