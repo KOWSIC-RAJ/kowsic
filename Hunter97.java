86ab1ccbdf4cbc38cca54eceed5f8ce9f2eb2d10
KOWSIC-RAJ <kowsickraj17@gmail.com>
2017-08-07 11:51:24 +0100

    Add files via upload

---
+++ Hunter97.java
@@ -0,0 +1,11 @@
+package guvi;
+
+public class Hunter97 {
+	public static void main(String[] args) {
+		String BarksOnceCode="Smile";
+		String BarksTwiceCode="cries";
+		String BarksThriceCode="fear";
+		System.out.println("Alex needs to wake up morning with "+BarksThriceCode+" and "+BarksTwiceCode+" before he goes to school and returns home from school with "+BarksOnceCode);
+	}
+
+}
